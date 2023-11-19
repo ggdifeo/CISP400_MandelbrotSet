7cb8ff0841c2d679ece96b6c78b33d5208df4b81
Your Name <you@example.com>
2023-11-18 22:40:45 -0800

    added game screen

--- code/main.cpp
+++ code/main.cpp
@@ -13,7 +13,34 @@ using namespace std;
 
 
 int main() {
+    //grabs the desktop resolution
+    VideoMode desktopMode = VideoMode::getDesktopMode();
 
-cout << "Hello World!" << endl;
+    //Divides the screen's resolution by 2 to scale down the screen
+    unsigned int screenWidth = desktopMode.width / 2;
+    unsigned int screenHeight = desktopMode.height / 2;
+
+    //Creates the window 
+    RenderWindow window(VideoMode(screenWidth, screenHeight), "Mandlebrot Set");
+
+    while (window.isOpen()) 
+    {
+        Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed) 
+            {
+                window.close();
+            }
+
+            //closes window if esc key is pressed
+            if (Keyboard::isKeyPressed(Keyboard::Escape))
+		    {
+			    window.close();
+		    }
+        }
+    }
+    window.display();
 
 }
