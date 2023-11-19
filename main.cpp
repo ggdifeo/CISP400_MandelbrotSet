c20666d7b2dd7ccf8266f662ec33e38786df2380
Your Name <you@example.com>
2023-11-18 22:12:38 -0800

    added starter files per instructions

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// Gabriel DiFeo && Karissa Merrill 
+
+// Headers 
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+//Namespace Declarations
+using namespace sf;
+using namespace std;
+
+
+int main() {
+
+cout << "Hello World!" << endl;
+
+}
